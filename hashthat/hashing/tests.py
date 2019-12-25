5279eda40c99031f26933ea974c76dc6bdd2dde5
pkro <pkrombach@web.de>
2019-12-25 09:25:30 +0100

    first test passed

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
-
-# Create your tests here.
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# class can be any name
+class FunctionalTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert browser.page_source.find('install')
+    # use descriptive names for tests
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
+
